f7a0f0def6b91a5d1697e0a2b5d5777b9f0d1209
Susan Allen <sallen@eoas.ubc.ca>
2021-04-04 15:01:48 -0700

    Example yaml and example script_runner for ObsModel Files

---
+++ notebooks/PhysTuning/script_runner.sh
@@ -0,0 +1,8 @@
+#python -m comparison_script ObsModel_202007F.yaml ctd
+#python -m comparison_script ObsModel_202007F.yaml bot
+#python -m comparison_script ObsModel_202007F.yaml psf
+#python -m comparison_script ObsModel_202007F.yaml pugts
+python -m comparison_script ObsModel_202007F.yaml pug
+#python -m comparison_script ObsModel_202007F.yaml hplc
+#python -m comparison_script ObsModel_202007F.yaml ferry
+
